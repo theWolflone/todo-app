c106d6e789aa2309d89c0ece7dc3c6d79aa0bd71
Santiago Lobo <lobo09.slu@gmail.com>
2023-04-17 11:05:42 -0600

    Maquetación del proyecto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-// import logo from './logo.svg';
+import React from 'react';
+import {TodoCounter} from './todoCounter';
+import {TodoSearch} from './todoSearch';
+import { TodoList } from './todoList';
+import { TodoItem } from './todoItem';
+import { CreateTodoButton } from './createTodoButton';
+
 // import './App.css';
+
+const todos = [
+  { text: "Cortar cebolla", completed: false },
+  { text: "Tomar el curso de intro a React", completed: false },
+  { text: "Llorar con la llorona", completed: false },
+];
 
 function App() {
   return (
-   
+    <React.Fragment>
+      <TodoCounter />
+
+      <TodoSearch />
+      <TodoList>
+        {todos.map((todo) => (
+          <TodoItem key={todo.text} texst={todo.text}/>
+        ))}
+      </TodoList>
+
+      <CreateTodoButton />
+    </React.Fragment>
   );
 }
 
