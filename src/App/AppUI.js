67fdf8448e44b2b57e20fe85bdfece0f4313ce9c
Santiago Lobo <lobo09.slu@gmail.com>
2024-01-12 18:33:16 -0600

    Utilizando el hook useContext

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -7,28 +7,15 @@ import { TodoItem } from "../TodoItem";
 import { CreateTodoButton } from "../CreateTodoButton";
 
 function AppUI() {
+
+  const { error, loading, searchedTodos, completeTodo, deleteTodo} = React.useContext(TodoContext);
+
   return (
     <React.Fragment>
       <TodoCounter />
 
       <TodoSearch />
-
-      <TodoContext.Consumer>
-        {/* <TodoList>
-        {error && <p>Desespérate, hubo un error...</p>}
-        {loading && <p>Estamos cargando, no desesperes...</p>}
-        {!loading && !searchedTodos.length && <p>¡Crea tu primer TODO!</p>}
-        {searchedTodos.map((todo) => (
-          <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList> */}
-        {({ error, loading, searchedTodos, completeTodo, deleteTodo }) => (
+       
           <TodoList>
             {error && <p>Desespérate, hubo un error...</p>}
             {loading && <p>Estamos cargando, no desesperes...</p>}
@@ -43,8 +30,6 @@ function AppUI() {
               />
             ))}
           </TodoList>
-        )}
-      </TodoContext.Consumer>
 
       <CreateTodoButton />
     </React.Fragment>
